dbcbb731b4ddd814287da81975d0d9489bef9001
Nafeu Nasir <nafeu.nasir@gmail.com>
2020-11-05 20:02:15 -0500

    Add pagination support for songs endpoint

--- server/api/v1/songs/index.js
+++ server/api/v1/songs/index.js
@@ -3,8 +3,8 @@ import schema from "./schema";
 import { GENIUS_API_URL } from "../constants";
 import { getAxiosConfig } from "../helpers";
 
-export const buildGeniusSongsByArtistIdRequest = ({ artistId }) => {
-  return `${GENIUS_API_URL}/artists/${artistId}/songs`;
+export const buildGeniusSongsByArtistIdRequest = ({ artistId, page }) => {
+  return `${GENIUS_API_URL}/artists/${artistId}/songs${page ? `?page=${page}` : ''}`;
 };
 
 export const getSongsList = ({ songsResults }) => {
@@ -23,8 +23,9 @@ export const getSongsList = ({ songsResults }) => {
 export const getSongs = async (req, res, next) => {
   try {
     const { artistId } = req.params;
+    const { page } = req.query;
 
-    const input = { artistId };
+    const input = { artistId, page };
 
     await schema.validateAsync(input);
 
--- server/api/v1/songs/schema.js
+++ server/api/v1/songs/schema.js
@@ -2,4 +2,5 @@ import Joi from "joi";
 
 export default Joi.object({
   artistId: Joi.number().integer().required(),
+  page: Joi.number().integer().optional()
 });
